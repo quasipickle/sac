f0022bb7ca9754fdad03149b232fe8f86f0d5dc8
Parley Martins <parleypachecomartins@gmail.com>
2016-01-24 17:32:31 -0700

    Add bootstrap integration

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,13 @@ var elixir = require('laravel-elixir');
  |
  */
 
+// elixir(function(mix) {
+//     mix.sass('app.scss');
+// });
 elixir(function(mix) {
-    mix.sass('app.scss');
+  var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+  mix.sass('app.scss')
+    .copy(bootstrapPath + '/fonts', 'public/fonts')
+    .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js');
+  mix.version(['public/css/app.css']);
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,105 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+  </head>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+  <body role="document">
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Bootstrap theme</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
         </div>
-    </body>
+      </nav>
+
+      <div class="container theme-showcase" role="main">
+
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="jumbotron">
+          <h1>Theme example</h1>
+          <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+        </div>
+
+
+        <div class="page-header">
+          <h1>Buttons</h1>
+        </div>
+        <p>
+          <button type="button" class="btn btn-lg btn-default">Default</button>
+          <button type="button" class="btn btn-lg btn-primary">Primary</button>
+          <button type="button" class="btn btn-lg btn-success">Success</button>
+          <button type="button" class="btn btn-lg btn-info">Info</button>
+          <button type="button" class="btn btn-lg btn-warning">Warning</button>
+          <button type="button" class="btn btn-lg btn-danger">Danger</button>
+          <button type="button" class="btn btn-lg btn-link">Link</button>
+        </p>
+        <p>
+          <button type="button" class="btn btn-default">Default</button>
+          <button type="button" class="btn btn-primary">Primary</button>
+          <button type="button" class="btn btn-success">Success</button>
+          <button type="button" class="btn btn-info">Info</button>
+          <button type="button" class="btn btn-warning">Warning</button>
+          <button type="button" class="btn btn-danger">Danger</button>
+          <button type="button" class="btn btn-link">Link</button>
+        </p>
+        <p>
+          <button type="button" class="btn btn-sm btn-default">Default</button>
+          <button type="button" class="btn btn-sm btn-primary">Primary</button>
+          <button type="button" class="btn btn-sm btn-success">Success</button>
+          <button type="button" class="btn btn-sm btn-info">Info</button>
+          <button type="button" class="btn btn-sm btn-warning">Warning</button>
+          <button type="button" class="btn btn-sm btn-danger">Danger</button>
+          <button type="button" class="btn btn-sm btn-link">Link</button>
+        </p>
+        <p>
+          <button type="button" class="btn btn-xs btn-default">Default</button>
+          <button type="button" class="btn btn-xs btn-primary">Primary</button>
+          <button type="button" class="btn btn-xs btn-success">Success</button>
+          <button type="button" class="btn btn-xs btn-info">Info</button>
+          <button type="button" class="btn btn-xs btn-warning">Warning</button>
+          <button type="button" class="btn btn-xs btn-danger">Danger</button>
+          <button type="button" class="btn btn-xs btn-link">Link</button>
+        </p>
+
+
+      </div> <!-- /container -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
+  </body>
 </html>
