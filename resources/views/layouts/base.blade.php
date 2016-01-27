5299c270673419840476ff65dcd5ef9d1f08925b
Parley Martins <parleypachecomartins@gmail.com>
2016-01-26 23:37:36 -0700

    Add base template for future development. Logos UofA

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head
+    content must come *after* these tags -->
+
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <title> SAC - @yield('title')</title>
+  </head>
+
+  <body>
+    @include('layouts.uofabar')
+
+    <div class="container-fluid">
+      @yield('content')
+    </div> <!-- /container -->
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
+  </body>
+</html>
---
+++ resources/views/layouts/uofabar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default uofa">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Augustana" class="uofalogo" >
+      </a>
+    </div>
+    <p class="navbar-text navbar-right">UofA Links</p>
+  </div>
+</nav>
