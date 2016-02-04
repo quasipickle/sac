53919a940cf9e47cf77901c8747560d9d7666ed3
Andrew Kaliel <agkaliel@gmail.com>
2016-02-04 15:16:03 -0700

    Added laravel authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,3 +2,9 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('signup', 'UsersController@create');
 Route::post('welcome', 'UsersController@store');
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
