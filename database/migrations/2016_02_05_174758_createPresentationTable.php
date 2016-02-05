be5ed9008d95415fdd886d9cdee8aa168bc71731
Andrew Kaliel <agkaliel@gmail.com>
2016-02-05 11:34:31 -0700

    created tables for presenatations, courses and types

---
+++ database/migrations/2016_02_05_174758_createPresentationTable.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresentationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentations', function (Blueprint $table){
+          $table->increments('id');
+          $table->string('student_name');
+          $table->string('professor_name');
+          $table->string('course');
+          $table->string('title');
+          $table->integer('type')->unsigned();
+          $table->text('abstract');
+          $table->text('special_notes');
+          $table->boolean('submitted');
+          $table->boolean('approved');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('presentations');
+    }
+}
---
+++ database/migrations/2016_02_05_180104_createCoursesTable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->string('description');
+            $table->primary('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('courses');
+    }
+}
