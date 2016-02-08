c36df12d2475385356d93b940778ca9607212d2a
Parley Martins <parleypachecomartins@gmail.com>
2016-02-08 14:50:25 -0700

    File to work on the import of other things from Open Data

--- import.php
+++ import.php
@@ -17,29 +17,6 @@ $term = getRange($conn, "Winter");
 
 // only continue if terms were found.  No terms means no new file generation
 if($term){
-	// $term_context = "term={$term},ou=calendar,dc=ualberta,dc=ca";
-
-	// # objects we want to retrieve
-	// $filter = "(facultycode=AU)";
-
-	// # Data we want
-	// # 'subject' is basically the discipline (ie: AUPOL)
-	// # 'catalog' is the number (ie: 104)
-	// # 'asstring' is the course name (ie: AUPOL 104)
-	// $attributes =  array('asstring', 'subject', 'catalog');
-
-	// # Some contexts might not exist yet in IMS, which throws an error
-	// $results = @ldap_list($conn,$term_context,$filter,$attributes);
-
-	// if($results)
-	// {
-	// 	$entries = ldap_get_entries($conn,$results);
-	// 		print_r($entries);
-	// 	# remove first "count" element
-	// 	array_shift($entries);
-	// }
-
-
 
 	// $class_context = "course=096036,term=".$term.",ou=calendar,dc=ualberta,dc=ca";
 	// $attributes = ['class', 'instructorUid', 'asString', 'classStatus'];
@@ -55,7 +32,7 @@ if($term){
 	// 	# remove first "count" element
 	// 	array_shift($entries);
 	// }
-
+//$term=1540 #Winter 2016
 	$context = "ou=people,dc=ualberta,dc=ca";
 	// $attributes = ['class', 'instructorUid', 'asString', 'classStatus'];
 	# Some contexts might not exist yet in IMS, which throws an error
@@ -69,40 +46,6 @@ if($term){
 		# remove first "count" element
 		array_shift($entries);
 	}
-
-}
-
-#
-# Function: getRange()
-# Purpose: To get the years & terms we are retrieving.  Always retrieves fall,winter,spring,summer
-#		   May start from Fall of the previous calendar year if early enough in the calendar year.
-# Returns: An array with 'terms' and 'years' items.  The 'terms' items has term IDS for the terms we want to retrieve
-#		   FALSE if something went wrong.
-#
-function getRange($conn, $term_name)
-{
-	$now = time();
-	$year = date('Y',$now);
-	$month = date('m',$now);
-
-	$termtitle = $term_name.'*'.$year;
-
-	$context = 'ou=calendar,dc=ualberta,dc=ca';
-	$filter = '(termtitle='.$termtitle.')';
-	$attributes = ['term'];
-	//get the term id
-	$results = @ldap_list($conn, $context, $filter, $attributes);
-
-	if($results){
-		$entries = @ldap_get_entries($conn,$results);
-
-		if($entries)
-			return $entries[0]['term'][0];
-		else
-			return FALSE;
-	}
-	else
-		return FALSE;
 
 }
 
