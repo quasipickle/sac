d1731d92bca3ab59a65ff6c44f012cd6e3cb6c39
Parley Martins <parleypachecomartins@gmail.com>
2016-02-15 14:13:01 -0700

    Add js files to assets

--- resources/views/basePages/sac.blade.php
+++ resources/views/basePages/sac.blade.php
@@ -19,7 +19,6 @@
     </div> <!-- /container -->
 
     @include('basePages.uofafooter')
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
+    <script src="{{ asset('js/all.js') }}"></script>
   </body>
 </html>
