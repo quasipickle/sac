7d4e769694ee3562a2246b556f999484a47a5f11
Parley Martins <parleypachecomartins@gmail.com>
2016-02-15 14:12:39 -0700

    Remove unused files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,6 @@
 <?php
-Route::get('/', 'StaticPagesController@home');
-
-
 Route::group(['middleware' => 'web'], function () {
+	Route::get('/', 'StaticPagesController@home');
     Route::auth();
 	Route::get('/presentation/new', 'PresentationsController@create');
-
-    Route::get('/home', 'HomeController@index');
 });
