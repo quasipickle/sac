e9715f5d5855bc3ba178b001b38f6f1f62221fcf
Parley Martins <parleypachecomartins@gmail.com>
2016-02-15 21:20:37 -0700

    Rename migration files (convention and standard)

--- database/migrations/2016_02_05_174758_create_presentations_table.php
+++ database/migrations/2016_02_05_174758_create_presentations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePresentationTable extends Migration
+class CreatePresentationsTable extends Migration
 {
     /**
      * Run the migrations.
