8cd467d0e67e4b1443b89cf45a621d2e7d6ef0ac
Parley Martins <parleypachecomartins@gmail.com>
2016-02-16 20:04:28 -0700

    Add font awesome to the project

--- resources/views/basePages/sac.blade.php
+++ resources/views/basePages/sac.blade.php
@@ -8,6 +8,8 @@
     content must come *after* these tags -->
 
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
     <title> SAC - @yield('title')</title>
   </head>
 
