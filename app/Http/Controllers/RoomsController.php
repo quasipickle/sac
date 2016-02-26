1609de4de6714057038fb3f881f80ccd5165feb4
Andrew Kaliel <agkaliel@gmail.com>
2016-02-26 05:05:24 -0700

    still in the process of adding room input to the database

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use Request;
 use App\Room;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -10,17 +11,20 @@ use App\Http\Controllers\Controller;
 class RoomsController extends Controller
 {
     public function show(){
-      return view('dashboard.room');
+      $rooms= Room::all();
+      return view('dashboard.room')->with('rooms', $rooms);
     }
     public function create(){
       return view('dashboard.addRoom');
     }
 
     public function store(){
-      $room = new App\Room();
-      $room->code = $code;
-      $room->description = $description;
-      $room->save();
-      return view('dashboard.room');
+       $input = Request::all();
+       $room = new Room();
+      //  $room->code = $input['code'];
+      //  $room->description = $input['description'];
+      //  $room->save();
+      return $input;
+      //return view('dashboard.room');
     }
 }
