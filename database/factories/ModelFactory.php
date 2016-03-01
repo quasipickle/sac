7cf0f844773d4be9a3954b2edae4da3326d51989
Parley Martins <parleypachecomartins@gmail.com>
2016-02-29 21:13:29 -0700

    Add new sample test and factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
+        'username' => $faker->username,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->defineAs(App\User::class, 'admin', function (Faker\Generator $faker) {
+	$user = $factory->raw(App\User::class);
+
+	$user->role = "admin";
+	return $user;
 });
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,28 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+
+
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testHomeBasic()
     {
         $this->visit('/')
              ->see('SAC Home Page');
+    }
+
+
+    public function testHomeLogin()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->withSession(['foo' => 'bar'])
+             ->visit('/')
+             ->see('Presentations');
     }
 }
