3d0edded396161e1ddf82b924a8227c58c628d59
Debbie <detsenak@ualberta.ca>
2016-03-03 16:42:02 -0700

    fixed conflict in roomsController

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use Auth;
-use App\Room;
-=======
 //use Illuminate\Http\Request;
 use Request;
 use App\Room;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
->>>>>>> 4511b0875923f8a02fa40923552fb05362dd0896
+
 
 class RoomsController extends Controller
 {
