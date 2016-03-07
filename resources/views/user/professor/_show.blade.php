be28651eabc6af4fc02f45f88d55c76d075b823c
Parley Martins <parleypachecomartins@gmail.com>
2016-03-07 15:28:36 -0700

    Add message for no courses

--- resources/views/user/professor/_show.blade.php
+++ resources/views/user/professor/_show.blade.php
@@ -1,17 +1,18 @@
 <div class="col-md-10 col-md-offset-1">
-	@foreach(Auth::user()->courses as $course)
-	<h4>
-		<a  role="button" data-toggle="collapse"
-			href="#{{$course->id}}" aria-expanded="false"
-			aria-controls="collapseExample">
-			{{ $course ->title }}
-		</a>
-	</h4>
+	@forelse(Auth::user()->courses as $course)
+		<h4>
+			<a  role="button" data-toggle="collapse"
+				href="#{{$course->id}}" aria-expanded="false"
+				aria-controls="collapseExample">
+				{{ $course ->title }}
+			</a>
+		</h4>
 
-	<div class="collapse" id="{{$course->id}}">
-		@include('user._presentations_table',
-			['presentations' => $course->presentations])
-	</div>
-	
-	@endforeach
+		<div class="collapse" id="{{$course->id}}">
+			@include('user._presentations_table',
+				['presentations' => $course->presentations])
+		</div>
+	@empty
+		Start by adding courses to your profile.
+	@endforelse
 </div>
