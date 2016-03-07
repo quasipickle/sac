2c2e599cda9408e322abc05984e100ade5b08fad
Parley Martins <parleypachecomartins@gmail.com>
2016-03-07 10:42:13 -0700

    Add professors view by course

--- resources/views/user/professor/_show.blade.php
+++ resources/views/user/professor/_show.blade.php
@@ -1,3 +1,16 @@
-<div>
-	PROFESSOR VIEW
+<div class="col-md-10 col-md-offset-1">
+	@foreach(Auth::user()->courses as $course)
+	<h4>
+		<a  role="button" data-toggle="collapse"
+			href="#{{$course->id}}" aria-expanded="false"
+			aria-controls="collapseExample">
+			{{ $course ->title }}
+		</a>
+	</h4>
+
+	<div class="collapse" id="{{$course->id}}">
+		@include('user._presentations_table', ['presentations' => $course->presentations])
+	</div>
+	
+	@endforeach
 </div>
