3e1411669ec1f7c4189c2991a7d3c598c83b55e3
Parley Martins <parleypachecomartins@gmail.com>
2016-03-08 17:47:23 -0700

    Update sidebar

--- resources/views/user/admin/_sidebar.blade.php
+++ resources/views/user/admin/_sidebar.blade.php
@@ -1,10 +1,10 @@
 <nav class="nav-sidebar">
     <ul class="nav">
+        <li><a href="#">Schedule</a></li>
         <li><a href="{{ route('room.index') }}">Rooms</a></li>
         <li><a href="{{ route('presentations') }}">Presentations</a></li>
-        <li><a href="#">Students</a></li>
         <li><a href="{{ route('courses') }}">Courses</a></li>
-        <li><a href="#">Schedule</a></li>
-        <li><a href="{{ route('requests') }}">Approve professor requests</a></li>
+        <li><a href="{{ route('requests') }}">Change role requests</a></li>
+        <li><a href="#">Presentation requests</a></li>
     </ul>
 </nav>
