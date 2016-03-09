6fa2ec8bec50889d8fb2086e1b56b413bf4a6db3
Parley Martins <parleypachecomartins@gmail.com>
2016-03-08 18:06:59 -0700

    Add item to sidebar

--- resources/views/user/admin/_sidebar.blade.php
+++ resources/views/user/admin/_sidebar.blade.php
@@ -2,9 +2,10 @@
     <ul class="nav">
         <li><a href="#">Schedule</a></li>
         <li><a href="{{ route('room.index') }}">Rooms</a></li>
-        <li><a href="{{ route('presentations') }}">Presentations</a></li>
+        <li><a href="{{ route('presentation.index') }}">All Presentations</a></li>
         <li><a href="{{ route('courses') }}">Courses</a></li>
         <li><a href="{{ route('requests') }}">Change role requests</a></li>
         <li><a href="#">Presentation requests</a></li>
+        <li><a href="#">OUR Nominees</a></li>
     </ul>
 </nav>
