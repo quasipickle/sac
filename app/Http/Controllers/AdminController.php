ee836247b1ad5510f958aee06be9cf297b03f6e4
Debbie <detsenak@ualberta.ca>
2016-03-20 16:07:47 -0600

    added new method for selecting courses

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Presentation;
 use App\Course;
 use App\User;
+use App\Term;
 
 class AdminController extends Controller
 {
@@ -19,6 +20,11 @@ class AdminController extends Controller
     public function view_courses(){
       $courses = Course::all();
       return view('courses.index')->with('courses', $courses);
+    }
+
+    public function select_term(){
+      $terms = Term::all();
+      return view('user.admin.select_term')->with('terms', $terms);
     }
 
 }
